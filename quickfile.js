9a7a7e908cb31adbc5a839ed39251ec87d81f8fc
BananaWanted <i@BananaWanted.com>
2015-02-25 03:46:15 +0800

    bug fix

--- quickfile.js
+++ quickfile.js
@@ -6,7 +6,7 @@
 * (the same license as Phonegap/Cordova itself). See the NOTICE
 * file for details.
 */
-var GapFile = {
+var QuickFile = {
 		extractDirectory: function(path){
 		var dirPath;
 		var lastSlash = path.lastIndexOf('/');
@@ -40,103 +40,112 @@ var GapFile = {
 	},
 	
 	llWriteFile: function(fullpath,data,success,fail,append){
-		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
-			function(fileSystem){
-				fileSystem.root.getDirectory(GapFile.extractDirectory(fullpath), {create: true, exclusive: false},
-				function(dirEntry){
-					dirEntry.getFile(GapFile.extractFilename(fullpath), {create: true}, 
-					function(fileEntry){
-							var fileURL = fileEntry.toURL();
-							fileEntry.createWriter(
-								function(writer){
-									writer.onwrite = function(evt){
-										success(fileURL);
-									};
-									writer.onerror = function(evt){ 
-										fail(evt.target.error);
-									};
-									if(append == true){
-										writer.seek(writer.length);
-									}
-									writer.write(data);
-							},fail);
+		window.resolveLocalFileSystemURL(QuickFile.extractDirectory(fullpath),
+		function(dirEntry){
+			dirEntry.getFile(QuickFile.extractFilename(fullpath), {create: true}, 
+			function(fileEntry){
+					var fileURL = fileEntry.toURL();
+					fileEntry.createWriter(
+						function(writer){
+							writer.onwrite = function(evt){
+								success(fileURL);
+							};
+							writer.onerror = function(evt){ 
+								fail(evt.target.error);
+							};
+							if(append == true){
+								writer.seek(writer.length);
+							}
+							writer.write(data);
 					},fail);
-				},fail);
 			},fail);
+		},fail);
 	},
 	readFile: function(fullpath,asText,success,fail){
-		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
-			function(fileSystem){
-				fileSystem.root.getDirectory(GapFile.extractDirectory(fullpath), {create: false, exclusive: false},
-				function(dirEntry){
-					dirEntry.getFile(GapFile.extractFilename(fullpath), {create: false}, 
-					function(fileEntry){
-						fileEntry.file(function(file){
-							var reader = new FileReader();
-							reader.onloadend = function(evt) {
-								success(evt.target.result);
-							};
-							reader.onerror =  function(evt){
-								fail();
-							}
-							if(asText){
-								reader.readAsText(file);
-							}
-							else{
-								reader.readAsDataURL(file);
-							}
-							},fail);
-						},fail);
-					},fail);
+		window.resolveLocalFileSystemURL(QuickFile.extractDirectory(fullpath),
+		function(dirEntry){
+			dirEntry.getFile(QuickFile.extractFilename(fullpath), {create: false}, 
+			function(fileEntry){
+				fileEntry.file(function(file){
+					var reader = new FileReader();
+					reader.onloadend = function(evt) {
+						success(evt.target.result);
+					};
+					reader.onerror =  function(evt){
+						fail();
+					}
+					if(asText){
+						reader.readAsText(file);
+					}
+					else{
+						reader.readAsDataURL(file);
+					}
 				},fail);
+			},fail);
+		},fail);
 	},
 	
 	deleteFile: function(fullpath,success,fail){
-			window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
-				function(fileSystem){
-					fileSystem.root.getDirectory(GapFile.extractDirectory(fullpath), {create: false, exclusive: false},
-					function(dirEntry){
-						dirEntry.getFile(GapFile.extractFilename(fullpath), {create: false}, 
-						function(fileEntry){
-							fileEntry.remove(success,fail);
-						},fail);
-					},fail);
+		window.resolveLocalFileSystemURL(QuickFile.extractDirectory(fullpath),
+			function(dirEntry){
+				dirEntry.getFile(QuickFile.extractFilename(fullpath), {create: false}, 
+				function(fileEntry){
+					fileEntry.remove(success,fail);
 				},fail);
-	},
-	readDirectory: function(dirName,success,fail){
-		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
-			function(fileSystem){
-				fileSystem.root.getDirectory(dirName, {create: false, exclusive: false},
-					function(dirEntry){
-						var directoryReader = dirEntry.createReader();
-						directoryReader.readEntries(
-							function(entries){
-								var flist = [];
-								for(var i = 0; i < entries.length; i++){
-									flist.push(entries[i].name);
-								}
-								success(flist);
-							},fail);
-					},fail);
-			},fail);       
-	},
-	
-	
-	mkDirectory: function(dirName,success,fail){
-		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
-			function(fileSystem){
-		        fileSystem.root.getDirectory(dirName, {create: true, exclusive: false}, success, fail);
 			},fail);
 	},
-	
-	rmDirectory: function(dirName,success,fail){
-		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
-			function(fileSystem){
-		        fileSystem.root.getDirectory(dirName, {create: false, exclusive: false}, 
-						function(dirEntry){
-							dirEntry.remove(success,fail);
-						},fail);
-				},fail)
+	readDirectory: function(dirFullPath,success,fail){
+		window.resolveLocalFileSystemURL(dirFullPath,
+			function(dirEntry){
+				var directoryReader = dirEntry.createReader();
+				directoryReader.readEntries(
+					function(entries){
+						var flist = [];
+						for(var i = 0; i < entries.length; i++){
+							flist.push(entries[i].name);
+						}
+						success(flist);
+					},fail);
+			},fail);    
+	},
+	mkDirectory: function(dirFullPath,success,fail){
+		var existPath = QuickFile.extractDirectory(dirFullPath);
+		var createPath = "";
+		function shiftPath(e, fail) {
+			if (e.code == FileError.NOT_FOUND_ERR) {
+				existPath = QuickFile.extractDirectory(existPath);
+				createPath = dirFullPath.replace(existPath, "");
+				createPath = QuickFile.extractDirectory(createPath);
+				createPath = createPath.substr(1);
+				getEntry(existPath);
+			} else {
+				fail(e);
+			}
+		};
+		function finish(entry) {
+			if (createPath == "") {
+				return;
+			}
+			if (createPath.indexOf('/') === -1) {
+				entry.getDirectory(createPath, {create: true, exclusive: false}, success, fail);
+			} else {
+				createPath = createPath.substring(0, createPath.indexOf('/'));
+				entry.getDirectory(createPath, {create: true, exclusive: false}, function(){
+					QuickFile.mkDirectory(dirFullPath, success, fail);
+				}, fail);
+			}
+		    
+		};
+		function getEntry(path) {
+			window.resolveLocalFileSystemURL(path, finish, shiftPath);
+		};
+		getEntry(dirFullPath);
+	},
+	rmDirectory: function(dirFullPath,success,fail){
+        window.resolveLocalFileSystemURL(dirFullPath,
+				function(dirEntry){
+					dirEntry.removeRecursively(success,fail);
+		},fail);
 	},
 
 	fileExists: function(fullpath,success,fail){
@@ -152,7 +161,6 @@ var GapFile = {
 			success(false);
 		},fail); 
 	},
-
 	errorToString: function(e) {
 		var msg = 'UNKNOWN_ERROR';
 
@@ -163,8 +171,3 @@ var GapFile = {
 		return msg;
 	}
 };
-
-if(module){
-	module.exports = GapFile;
-}
-
